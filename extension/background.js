9f16c6832f541fde4740eb2688ac0ab160f4768a
Arthur Kurath <akurath@swissonline.ch>
2016-03-15 15:53:39 +0100

    Initial commit

---
+++ extension/background.js
@@ -0,0 +1,23 @@
+﻿var extension_index_url = 'chrome-extension://'+location.host+'/index.html';
+function isExtensionURL(url) {
+  if(url == extension_index_url) {
+    return true;
+  }
+  return false;
+}
+// Find options page in all opened tabs
+function goToOrOpenExtension() {
+  chrome.tabs.getAllInWindow(undefined, function(tabs) {
+    for (var i = 0, tab; tab = tabs[i]; i++) {
+      if (tab.url && isExtensionURL(tab.url)) {
+        chrome.tabs.update(tab.id, {selected: true});
+        return;
+      }
+    }
+    chrome.tabs.create({url: extension_index_url});
+  });
+}
+// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  goToOrOpenExtension();
+});
