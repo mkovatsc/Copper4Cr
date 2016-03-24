f215a2ba22947ec087a1085bf7e361e4f068e434
Arthur Kurath <akurath@swissonline.ch>
2016-03-24 16:45:49 +0100

    Message type

---
+++ app/testing/tests/coap/CoapMessageTypeTest.js
@@ -0,0 +1,14 @@
+QUnit.test("getMessageType", function(assert) {
+	assert.deepEqual(Copper.CoapMessage.Type.CON, Copper.CoapMessage.Type.getType(0));
+	assert.deepEqual(Copper.CoapMessage.Type.ACK, Copper.CoapMessage.Type.getTypeForName("ACK"));
+
+	assert.throws(function(){
+		Copper.CoapMessage.Type.getType(-1)
+	});
+	assert.throws(function(){
+		Copper.CoapMessage.Type.getType("10");
+	});
+	assert.throws(function(){
+		Copper.CoapMessage.Type.getTypeForName("CONFIRM");
+	});
+});
