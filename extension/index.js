d0caff877684438779eacf2ff493b20738cd279d
hagedoch <hagedoch@student.ethz.ch>
2016-04-26 17:15:30 +0200

    Added Coap Message Log Panel, Scroll support and better layouts (incl. padding, margin)

--- extension/index.js
+++ extension/index.js
@@ -1,19 +1,4 @@
-﻿// Payload dropdown
-$('#payload-dropdown .dropdown-menu a').on('click', function(){    
-    $('#payload-dropdown .dropdown-toggle').html($(this).html() + ' <span class="caret"></span>');    
-})
-
-$(document).on('click', '.dropdown-menu.dropdown-menu-form', function(e) {
-  e.stopPropagation();
-});
-
-var top_container = document.getElementById('top-container');
-var sub_container = document.getElementById('sub-container');
-var sidebar_right = document.getElementById('sidebar-right');
-var sidebar_left = document.getElementById('sidebar-left');
-
-console.log(top_container.width);
-
+﻿
 function onClickDiscover()
 {
 }
