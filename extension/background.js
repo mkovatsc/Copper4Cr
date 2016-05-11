92929f91677fcf8a4231a0fd7a50bbb1de8c1c26
Arthur Kurath <akurath@swissonline.ch>
2016-05-11 14:13:04 +0200

    clean up

--- extension/background.js
+++ extension/background.js
@@ -1,39 +1,4 @@
-﻿$.fn.extend({
-  treed: function() {
-    return this.each(function() {
-        //initialize each of the top levels
-        var tree = $(this);
-        tree.addClass("tree");
-    	tree.find('li').has("ul").each(function () {
-            var branch = $(this); //li with children ul
-            branch.prepend("<i class='indicator glyphicon glyphicon-plus-sign'></i>");
-			branch.addClass('branch');
-			branch.on('click', function (e) {
-                if (this == e.target) {
-                    var icon = $(this).children('i:first');
-                    icon.toggleClass("glyphicon-minus-sign glyphicon-plus-sign");
-                    $(this).children().children().toggle();
-                }
-            })
-			branch.children().children().toggle();
-        });
-		//fire event from the dynamically added icon
-        $('.branch .indicator').on('click',function(){
-            $(this).closest('li').click();
-        });
-		//fire event to open branch if the li contains an anchor instead of text
-        $('.branch a').on('click',function(e){
-            $(this).closest('li').click();
-            e.preventDefault();
-        });
-        //fire event to open branch if the li contains a button instead of text
-        $('.branch button').on('click',function(e){
-            $(this).closest('li').click();
-            e.preventDefault();
-        });
-    }); 
-  }
+﻿﻿// Called when the user clicks on the browser action.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({url: chrome.extension.getURL('index.html')});
 });
-
-
-$('.tree').treed();
